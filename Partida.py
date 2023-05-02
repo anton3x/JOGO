bc5ea7809862f63c9285d6676fcbeb9bf94d3a49
josed <antonio15681@icloud.com>
2023-05-02 15:18:19 +0100

    alteracao

--- Partida.py
+++ Partida.py
@@ -1,8 +1,10 @@
 import random
 def partida():
     nome = input("Nome do jogador: ")
-    numero = random.randint(0, 1)
+    numero = random.randint(0, 1) #quem comeca
     if numero == 0:
         print("Bot começa!")
     else:
         print("O %s começa!" % (nome))
+
+

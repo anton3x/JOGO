07b77fca88b91143c3e7bc8e5d3cd4d27638c647
zé <pedrobrasvr@gmail.com>
2023-05-09 13:22:33 +0100

    adicionei o codigo aos botoes

---
+++ Botao4.py
@@ -0,0 +1,23 @@
+import pygame
+def botao1():
+    fps = pygame.time.Clock()
+    largura = 1024
+    altura = 600
+
+    JogoLoop = True
+
+    tela = pygame.display.set_mode([largura, altura])
+    pygame.display.set_caption("Lucky Numbers")
+
+    ColorBack = {"azul": [0, 132, 252], "vermelho": [137, 28, 36]}
+
+    while JogoLoop:
+        fps.tick(60)
+        tela.fill(ColorBack["azul"])
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                JogoLoop = False
+
+
+            pygame.display.update()
+    pygame.quit()

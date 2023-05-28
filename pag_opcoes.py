5fb622b6328bfc131d7331b1a4156232a14eb26f
zé <pedrobrasvr@gmail.com>
2023-05-28 01:52:35 +0100

    alteração código

--- pag_opcoes.py
+++ pag_opcoes.py
@@ -455,12 +455,10 @@ def regras_jogo():
 
     # Cria a caixa de texto
     text_box = pygame_gui.elements.UITextBox(
-        html_text="<body><font color='#FFA500' face='Times New Roman'><b>Ideia de Jogo:</b></font> Cada jogador tenta ser o primeiro a preencher completamente o seu jardim com trevos. Mas eles devem verificar se os números estão organizados em ordem crescente em cada linha e coluna.</body>"
-                  "\n<font color='#FFA500' face='Arial'><b>Configuração do Jogo:</b></font> Usa um conjunto completo de trevos (= 1 cor numerada de 1 a 20) por jogador. Com menos de 4 jogadores, devolve as peças não utilizadas à caixa do jogo. \n Embaralha os trevos e coloca-os virados para baixo no meio da mesa. \n Cada jogador leva um jogo bordo e orienta-o para que a joaninha esteja no canto inferior direito.</body>"
-                  "\n<font color='#FFA500' face='Verdana'><b>GamePlay:</b></font> O jogador mais velho começa e, em seguida, o jogo prossegue no sentido horário. Na tua vez, deves escolher uma das duas opções a seguir:"
-                  "\n<font color='#FFA500' face='Comic Sans MS'><b>A) Take a face-down clover:</b></font> Pega um trevo virado para baixo do meio da mesa e coloca-o, virado para cima, no tabuleiro de jogo (vê Regras de Colocação à direita). Se não podes ou não queres colocá-lo, deixa-lo, virado para cima no meio da mesa."
-                  "\n<font color='#FFA500' face='Comic Sans MS'><b>B) Take a face-up clover:</b></font> Não reveles outro trevo. Em vez disso, pega um dos trevos virados para cima e adicioná-lo ao tabuleiro de jogo em de acordo com as Regras de Colocação</b></font>."
-                  "\n<font color='#FFA500' face='Comic Sans MS'><b>Regras de Colocação:</b></font> Podes adicionar um novo trevo a um espaço vazio no tabuleiro de jogo ou trocá-lo por um trevo colocado anteriormente (e devolver o trevo trocado no meio da mesa, com a face para cima)."
+        html_text="<body><font color='#FFA500' face='Times New Roman'><b>Ideia de Jogo:</b></font> Cada jogador tenta ser o primeiro a preencher completamente o seu tabuleiro com trevos e deve verificar se os números estão organizados em ordem crescente em cada linha e coluna.</body>"
+                  "\n<font color='#FFA500' face='Arial'><b>Estrutura do Jogo:</b></font> Prepara um conjunto completo de trevos por jogador. O máximo é 4 jogadores, se não utilizar as peças devolve à pilha.</body>"
+                  "\n<font color='#FFA500' face='Verdana'><b>GamePlay:</b></font> O jogador a começar é escolhido aleatoriamente, em seguida, o jogo prossegue no sentido horário. Na tua vez, para pegares um trevo deves escolher uma das duas opções a seguir: ir buscar à pillha ou à table (caso exita um trevo lá)."
+                  "\n<font color='#FFA500' face='Comic Sans MS'><b>Regras de Colocação:</b></font> Podes adicionar um novo trevo a um espaço vazio no tabuleiro de jogo ou trocá-lo por um trevo colocado anteriormente (e devolver o trevo trocado no meio da mesa)."
                   "\nO número do trevo que colocas no tabuleiro deve caber, em ordem crescente, com todos os outros números na sua linha e na sua coluna (mas os números em uma linha ou coluna não precisam seguir uns aos outros como 7,8,9...). "
                   "</body>",
         relative_rect=text_box_rect,
